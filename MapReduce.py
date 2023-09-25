6ec145fff93785919aadfbf319dd0581d7c0e7a0
Youssef Bakouny <Youssef.Bakouny@usj.edu.lb>
2023-09-25 11:52:07 +0300

    Implement fact using product

--- MapReduce.py
+++ MapReduce.py
@@ -1,26 +1,28 @@
 from functools import partial
+# def multiply(x, y, z):
+#     return x * y * z
+#
+# # create a new function that multiplies by 2
+# fp = partial(multiply, 2, 3)
+# print(fp(4))
 
+def sum(f, a, b):
+    if a > b:
+        return 0
+    else:
+        return f(a) + sum(f, a + 1, b)
 
-def sum(f) :
-    def helper(a, b) :
-        if a > b:
-            return 0
-        else:
-            return f(a) + helper(a + 1, b)
-    return helper
-
-sumInts = sum(lambda x : x)
-
-sumCubes = sum(lambda x : x**3)
-
-def fact(n) :
-    if n == 0 :
+def product(f, a, b):
+    if a > b:
         return 1
     else:
-        return n * fact(n-1)
+        return f(a) * product(f, a + 1, b)
 
-sumFact = sum(fact)
 
-print(sumCubes(0,50))
-print(sumFact(0,5))
-print(sumInts(0,5))
+sumInts = partial(sum, lambda x: x)
+sumCubes = partial(sum, lambda x: x ** 3)
+
+def fact(n) :
+    return product(lambda x : x, 1, n)
+
+sumFact = partial(sum, fact)
